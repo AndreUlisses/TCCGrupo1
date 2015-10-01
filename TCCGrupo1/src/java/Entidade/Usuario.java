e5192106a095b1c574b899be2b819bc72e05ac1e
Grazi <Grazi@Grazi-PC>
2015-10-01 17:41:10 -0300

    Projeto atualizado

--- TCCGrupo1/src/java/Entidade/Usuario.java
+++ TCCGrupo1/src/java/Entidade/Usuario.java
@@ -1,11 +1,54 @@
 package Entidade;
 
+import java.util.Date;
+
+
+
 public class Usuario {
-    
+
     private Integer id;
+    private String nome;
+    private Date dataNascimento;
+    private boolean sexo;
+    private String user;
     private String email;
     private String senha;
+    
 
+        public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    //Usuário, senha, e-mail, nome, sobrenome, data de nascimento, sexo, foto do perfil.
+    public void setUser(String user) {
+        this.user = user;
+    }
+    
+    
     public Integer getId() {
         return id;
     }
