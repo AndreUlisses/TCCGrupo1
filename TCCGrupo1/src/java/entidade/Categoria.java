c31906d9edc5cccbf0ae6b49e8c6f2e6ad5679f5
Grazi <Grazi@Grazi-PC>
2015-10-15 10:40:39 -0300

    Projeto atualizado

---
+++ TCCGrupo1/src/java/entidade/Categoria.java
@@ -0,0 +1,36 @@
+package entidade;
+
+public class Categoria {
+   
+    private  int idCategoria;
+    private String Nome;
+    private  String descricao;
+    
+    //GET&SET
+    
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+}
--- TCCGrupo1/src/java/entidade/Localidade.java
+++ TCCGrupo1/src/java/entidade/Localidade.java
@@ -1,16 +1,65 @@
 package entidade;
 import java.sql.Date;
 
-//por herança para os ID'S
+
 public class Localidade {
 
     private int idLocalidade;
-    private int idCategoria;
-    private int idUsuario;
-    private String nome;
-    private String descricao;
+    private Categoria idCategoria;
+    private Usuario idUsuario;
+    private String nomeLocalidade;
+    private String descricaoLocalidade;
     private Date dtLocalidade;
                     
-    //fazer GET&SET
+    //GET&SET
+
+    public int getIdLocalidade() {
+        return idLocalidade;
+    }
+
+    public void setIdLocalidade(int idLocalidade) {
+        this.idLocalidade = idLocalidade;
+    }
+
+    public Categoria getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Categoria idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNomeLocalidade() {
+        return nomeLocalidade;
+    }
+
+    public void setNomeLocalidade(String nomeLocalidade) {
+        this.nomeLocalidade = nomeLocalidade;
+    }
+
+    public String getDescricaoLocalidade() {
+        return descricaoLocalidade;
+    }
+
+    public void setDescricaoLocalidade(String descricaoLocalidade) {
+        this.descricaoLocalidade = descricaoLocalidade;
+    }
+
+    public Date getDtLocalidade() {
+        return dtLocalidade;
+    }
+
+    public void setDtLocalidade(Date dtLocalidade) {
+        this.dtLocalidade = dtLocalidade;
+    }
+   
     
 }
